b9ff285567cedc95f0534ac5419480e906d4a6c5
tanishkapandey2006-afk <tanishkapandey2006@gmail.com>
2026-03-18 22:09:58 +0530

    Add files via upload

---
+++ LEARNING/Python/day4.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks: "))
+
+if marks>100 or marks<0:
+    print("Invalid marks!")
+elif marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+else:
+    print("Grade: D")
