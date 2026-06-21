ceeec1b03841ed7a17fd75247babcbcafff7a557
tanishkapandey2006-afk <tanishkapandey2006@gmail.com>
2026-06-21 15:31:19 +0530

    Refactor student management system with menu function

--- LEARNING/Python/student_management_system.py
+++ LEARNING/Python/student_management_system.py
@@ -1,11 +1,11 @@
 students = []
-
-while True:
+def display_menu():
     print("\n--- Student Management System ---")
     print("1. Add Student")
     print("2. View Students")
-    print("3. Exit")
+    print("3. Search Student")
     print("4. Delete Student Selected")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -18,10 +18,17 @@ while True:
     elif choice == "2":
         for i, student in enumerate(students, start=1):
             print(i, student["name"], "-", student["marks"])
+
+    elif choice== "3":
+        search_name = input("Enter student name to search: ")
+        for student in students:
+            if student["name"] == search_name:
+                print("Student found.")
+                print("Student Name: ",search_name,"\nStudent Marks: ",student["marks"])
+                break
+        else:
+            print("Student not found.")
     
-    elif choice == "3":
-        print("Exiting program...")
-        break
     elif choice == "4":
         stud=input("Enter student name to remove: ")
         for student in students:
@@ -31,6 +38,15 @@ while True:
                 break
         else:
             print("Student not found.")
+
+    elif choice == "5":
+        print("Exiting program...")
+        return False
     
     else:
         print("Invalid choice, try again")
+    return True
+while True:
+    if not display_menu():
+        break
+    
